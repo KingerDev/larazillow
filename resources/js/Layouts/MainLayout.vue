0c95e4b40473876a93c1348d70bd6a33566af253
ITMartinBalaz <108070028+ITMartinBalaz@users.noreply.github.com>
2023-10-18 17:35:09 +0200

    Notitifications back-end

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -9,6 +9,12 @@
                     <Link :href="route('listing.index')">LaraZillow</Link>
                 </div>
                 <div class="flex items-center gap-4" v-if="user">
+                    <Link :href="route('notification.index')" v-if="notificationCount" class="text-gray-500 relative pr-2 py-2 text-lg">
+                        🔔
+                        <div class="absolute right-0 top-0 w-5 h-5 bg-red-700 dark:bg-red-400 text-white font-medium border border-white dark:border-gray-900 rounded-full text-xs text-center">
+                            {{ notificationCount }}
+                        </div>
+                    </Link>
                     <Link :href="route('realtor.listing.index')" class="text-gray-500">{{ user.name }}</Link>
                     <Link :href="route('realtor.listing.create')" class="btn-primary">+ New Listing</Link>
                     <div>
@@ -42,5 +48,9 @@
 
     const user = computed(
         () => page.props.user,
+    )
+
+    const notificationCount = computed(
+        () => Math.min(page.props.user.notificationCount, 9),
     )
 </script>
--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -2,12 +2,14 @@
     <div>
         <ListingAddress :listing="listing" />
         <div class="flex flex-col-reverse md:grid md:grid-cols-12 gap-4">
-            <Box class="md:col-span-7 flex items-center">
-                <div v-if="listing.images.length" class="grid grid-cols-2 gap-1">
+            <Box v-if="listing.images.length" class="md:col-span-7 flex items-center">
+                <div class="grid grid-cols-2 gap-1">
                     <img v-for="image in listing.images" :key="image.id" :src="image.src" alt="image" />
                 </div>
-                <div v-else class="w-full text-center font-medium text-gray-500">No images</div>
             </Box>
+
+            <EmptyState v-else class="md:col-span-7 flex items-center">No images</EmptyState>
+
             <div class="md:col-span-5 flex flex-col gap-4">
                 <Box>
                     <template #header>
@@ -74,6 +76,7 @@ import {useMonthlyPayment} from "@/Composables/useMonthlyPayment.js";
 import MakeOffer from "@/Pages/Listing/Show/Components/MakeOffer.vue";
 import {usePage} from "@inertiajs/vue3";
 import OfferMade from "@/Pages/Listing/Show/Components/OfferMade.vue";
+import EmptyState from "@/Components/UI/EmptyState.vue";
 
 const interestRate = ref(2.5);
 const duration = ref(25);
